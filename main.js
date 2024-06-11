b39b3310658d3b846f20608bf7df8d00c15d13bd
Jayeon Hwang <jayeon512@gmail.com>
2024-06-11 15:02:16 -0500

    display items

--- main.js
+++ main.js
@@ -2,8 +2,26 @@
 function loadItems() {
   return fetch('data/data.json')
     .then(response => response.json())
-    .then(json => console.log(json));
+    .then(json => json.items);
+}
+
+function displayItems(items) {
+  const container = document.querySelector('.items');
+  container.innerHTML = items.map(item => createHTMLString(item)).join('');
+}
+
+function createHTMLString(item) {
+  return `
+  <li class="item">
+    <img src="${item.image}" alt="${item.type}" class="item_thumbnail">
+    <span class="item_description"> ${item.gender}, ${item.size} </span>
+  </li>
+  `;
 }
 
 loadItems()
-
+  .then(items => {
+    console.log(items);
+    displayItems(items);
+  })
+  .catch(console.log);
